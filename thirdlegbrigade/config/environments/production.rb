cc9131710fe13a9ac830380edd97bcd99323d1b1
Kevin Fox <kevin@kurotek.com>
2014-04-19 10:19:38 -0700

    kf - Who knows

--- thirdlegbrigade/config/environments/production.rb
+++ thirdlegbrigade/config/environments/production.rb
@@ -58,7 +58,7 @@ Thirdlegbrigade::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w[bootstrap.min.js bootstrap-responsive.min.css]
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
