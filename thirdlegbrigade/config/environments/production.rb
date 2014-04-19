d5b427bd6f6b5f448f58b0cb8a6f4e14c87021a7
Kevin Fox <kevin@kurotek.com>
2014-04-19 09:34:59 -0700

    kf - Easing Jabby's pussy

--- thirdlegbrigade/config/environments/production.rb
+++ thirdlegbrigade/config/environments/production.rb
@@ -1,4 +1,4 @@
-Thirdlegbrigrade::Application.configure do
+Thirdlegbrigade::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
